3f0f0cb3bd9d40a0686ef7bfbb6f8df4effe394c
Imdadul Haque Enan <ihenan1006@gmail.com>
2022-05-26 18:01:20 +0600

    connect to mongo

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.k2t5c.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const phonesCollection = client.db('eSmartInventory').collection('phones');
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
     console.log('e-smart-inventory server is running');
